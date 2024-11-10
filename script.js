5c2b3adbbe57a127f698a79f0e64f19c07a0db4c
WebWolf77 <tommyecom888@gmail.com>
2024-11-10 22:50:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
+// Select the menu button and navigation links elements
 const menuBtn = document.getElementById('menu-btn');
 const navLinks = document.getElementById('nav-links');
 const menuBtnIcon = menuBtn.querySelector('i');
 
+// Toggle the navigation menu and icon on click
 menuBtn.addEventListener("click",(e) => {
     navLinks.classList.toggle("open");
 
+    // Change the menu button icon based on the menu state
     const isOpen = navLinks.classList.contains("open");
     menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
 });
 
+// Close the navigation menu when a link is clicked
 navLinks.addEventListener("click", (e) => {
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
 
+// ScrollReveal configuration
 const scrollRevealOption = {
     distance: "50px",
     origin: "bottom",
     duration: 1000,
 };
 
+// Apply ScrollReveal animations to elements
 ScrollReveal().reveal(".header__image img", {
     ...scrollRevealOption,
     origin: "right",
@@ -32,12 +38,10 @@ ScrollReveal().reveal(".header__content h1", {
     ...scrollRevealOption,
     delay: 1000,
 });
-
 ScrollReveal().reveal(".order__card", {
     ...scrollRevealOption,
     interval: 500,
 });
-
 ScrollReveal().reveal(".event__content", {
     duration: 1000,
 });
